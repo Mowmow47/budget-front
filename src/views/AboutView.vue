23d1bd885333e78189d9793f75a08e2c3748500e
Mowmow47 <morgan.duprat@gmail.com>
2022-06-01 14:32:42 +0200

    Installation and configuration of tailwindcss, postcss and autoprefixer

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,22 +1,20 @@
 <script setup>
 import { ref, watchEffect } from "vue";
 
-const API_URL = `http://api.budget.lan/account`;
+const API_URL = `http://api.budget.lan/accounts`;
 const accounts = ref(null);
 
 watchEffect(async () => {
   accounts.value = await (await fetch(API_URL)).json();
 });
-
-console.log(accounts);
 </script>
 
 <template>
-  <h1>Lastest accounts from the api</h1>
+  <h1>Accounts list</h1>
   <ul>
     <li v-for="account in accounts">
-      <span class="title">{{ account.title }}</span
-      ><br />Contenu : {{ account.content }}
+      <span class="title">{{ account.name }} - {{ account.accountNumber }}</span
+      ><br />Banque : {{ account.bankName }} <br />Taux d'interêt : {{ account.interestRate }}
     </li>
   </ul>
 </template>
